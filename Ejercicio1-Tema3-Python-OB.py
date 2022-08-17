df27e7f01d1f89aedcc07ad172663fdf9a6edc0d
JuIgna <87208351+JuIgna@users.noreply.github.com>
2022-08-17 00:55:21 -0300

    Add files via upload

---
+++ Ejercicio1-Tema3-Python-OB.py
@@ -0,0 +1,22 @@
+# Ejercicio 1 del tema 3 del curso de Python -- OpenBootcamp
+nombre = "Juan"
+apellidos = "Perez"
+email = "juignavazquez@gmail.com"
+telefono = "125863251"
+casado = False
+hijos = False
+lista_amigos = ["Agustin", "Nicolas", "Pedro"]
+peliculas_vistas = {1:"Avengers", 2: "Toy Story", 3:"Saw"}
+
+print (nombre)
+print (apellidos)
+print (email)
+print (telefono)
+print (casado)
+print (hijos)
+print (lista_amigos)
+print (peliculas_vistas)
+
+input()
+
+
