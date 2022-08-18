a0eccf4a6c9a0e1908cec8621dd191205b9dc93d
JuIgna <87208351+JuIgna@users.noreply.github.com>
2022-08-18 13:34:59 -0300

    Add files via upload

---
+++ Ejercicio1-Tema5-Python-OB.py
@@ -0,0 +1,12 @@
+def area_triangulo (altura, base):
+    return (base*altura)/2
+
+def area_circulo (radio):
+    return 3.14*radio*radio
+
+
+
+
+print ("El area del rectangulo: ", area_triangulo(2,3))
+
+print ("El area del circulo: ", area_circulo(4))
